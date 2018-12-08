d5a98e514ee9b177bce06cc4b8649b2a091757c4
Michał Szostek <michaszo18@gmail.com>
2018-12-08 16:39:31 +0100

    aa

---
+++ index.js
@@ -0,0 +1,10 @@
+const beltCount = document.querySelector('.beltCount');
+const alkLevel = document.querySelector('.alkLevel');
+
+const kieliszek = document.querySelector('#kieliszek');
+const kieliszek = document.querySelector('#browar');
+const kieliszek = document.querySelector('#tequila');
+const kieliszek = document.querySelector('#telOdMamy');
+const kieliszek = document.querySelector('#telOdZiomka');
+const kieliszek = document.querySelector('#pizza');
+const kieliszek = document.querySelector('#kebab');
